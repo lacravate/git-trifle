e6b8e1ab5522d8e577c8d8e3e141ffa15a33d193
lacravate <lacravate@lacravate.fr>
2012-10-31 20:12:36 +0100

    very very fat commit; lots of repo' alterations, queries and manipulations; and a fatty fatty spec file

--- lib/git/trifle.rb
+++ lib/git/trifle.rb
@@ -64,29 +64,178 @@ module Git
       end
     end
 
-    def reset(*args)
-      # mere delegation here
-      layer.reset *args
+    def create_branch(name, options={})
+      layer.branch(name, options).create
     end
 
-    def working_directory
+    def delete_branch(branch)
+      # woodsman advice : dude, don't saw the branch you're on
+      return if branch == current_branch
+      layer.branch(branch).delete
+    end
+
+    def pull_all_branches(options={})
+      cover path: options[:path] if options[:path]
+
+      # freshen the repo'
+      fetch
+      # updates the currennt branch
+      pull remote_for(current_branch), current_branch
+      # creates a local branch for each on the remote
+      other_remote_branches.each do |branch|
+        local = branch.split('/').last
+        delete_branch local if has_local_branch? local
+        create_branch local, track: branch
+      end
+    end
+
+    def checkout(name, options={})
+      # avoid crash when repo' was just init'ed
+      return false unless any_commits?
+      # 0 but true
+      return true if name == current_branch
+
+      # -b option on command line
+      options.merge! new_branch: true unless has_branch? name
+
+      # wipe all options to let git to the job and checkout
+      # tracking branch. YKWIM ? then DWIM
+      options = {} if has_remote_branch? name
+      layer.checkout name, options
+    end
+
+    def checkout_w_branch(branch, options={})
+      # a git-trifle branch always stems from initial commit
+      # so enforce this if it does not exist yet
+      options.merge!(commit: initial_commit) unless has_branch? branch
+      checkout branch, options
+    end
+
+    def push_file(file, options={})
+      # yeah yeah i allow to push on another branch in one line
+      # not sure it's a good idea. We'll see...
+      options[:branch] ||= current_branch
+      options[:remote] ||= remote_for(options[:branch])
+      # not sure of the option name. Well...
+      options[:verb] ||= 'add'
+
+      # not always a good idea to actually perform
+      # a checkout here but i let checkout method know that
+      checkout options[:branch]
+      # add || rm
+      send options[:verb], file
+      commit "#{options[:verb]} #{file} (brought to you by #{self.class.name})"
+      # Run Forrest, run !
+      push options[:remote], options[:branch]
+    end
+
+    def commits(options={})
+      # yeah, reverse !
+      # Because it's only in the Bible that i understand
+      # why the first ones will be the last ones.
+      layer.log.object(options[:branch] || current_branch).map{ |commit| commit.sha }.reverse
+    end
+
+    def local_branches
+      layer.branches.local.map &:name
+    end
+
+    def remote_branches
+      layer.branches.remote.map &:name
+    end
+
+    def remote_for(branch)
+      # get out of here if i don't know you at all
+      return unless has_branch? branch
+      # ok you got me... I Perl'ed when i was younger
+      remote_branches.
+        map { |b| b.split '/' }. # returns a list of remote branch name elements
+        map { |elements| elements[-2] if elements.last == branch }. # list of nil or remote names
+        compact.first || remote_name
+    end
+
+    def remote_branch_for(branch)
+      # that one's even funnier
+      remote_branches.
+        map { |b| b.split '/' }. # returns a list of remote branch name elements
+        map { |elements| "#{elements[-2]}/#{elements[-1]}" if elements.last == branch }. # list of nil or remote branch names
+        compact.first
+    end
+
+    def remote_url(options={})
+      cover path: options[:path] if options[:path]
+      # yucky ? Maybe... But funny as well...
+      layer.remotes.select { |r| options[:name] ? r.name == options[:name] : true }.map(&:url).first
+    end
+
+    def remote_name(options={})
+      # yucky ? Maybe... But funny as well...
+      layer.remotes.select { |r| options[:url] ? r.url == options[:url] : true }.map(&:name).first
+    end
+
+    def other_remote_branches
+      # i like to make myself laugh as well
+      remote_branches.reject { |branch| branch == 'origin/HEAD' || branch == remote_branch_for(current_branch) }
+    end
+
+    def status(type=nil)
+      types = (type && STATUS_LIST.include?(type)) ? [type] : STATUS_LIST
+
+      # Status as Hash of arrays, keys are statuses
+      types.inject({}) do |s, type|
+        # i.g layer.status.changed and keys only to get the filenames
+        s.merge! type => layer.status.send(type).keys
+      end
+    end
+
+    def alterations(options={})
+      # which files have a status
+      # represented as in status above
+      cover path: options[:path] if options[:path]
+      status(options[:status]).select { |t, files| files.any? }
+    end
+
+    def files_with_status(s)
+      # i like being specific
+      status(s).values.flatten
+    end
+
+    def ls_tree(options={})
+      # do i use that ?
+      sha = options.delete(:sha) { current_branch }
+      layer.ls_tree(sha, options).values.map(&:keys).flatten
+    end
+
+    def directory
       # explanation here ?
       # Really ?
       layer.dir.to_s
     end
 
-    def remote(name=nil)
-      # yucky ? Maybe... But funny as well...
-      layer.remotes.select { |r| name ? r.name == name : true }.map(&:url).first
-    end
-
     def files_paths
-      # for now, don't need the Git::StatusFile
-      # only paths names
+      # for now, need only paths names
       layer.ls_files.keys
     end
 
-    def local_remotes?
+    def initial_commit
+      # get the repo' initial commit
+      # the trinary with master is probably useless
+      branch = has_branch?('master') ? 'master' : local_branches.first
+      commits(branch: branch).first
+    end
+
+    def wipe_directory!
+      # not too clever way to do that
+      # i remove directories but try to avoid
+      # removinng the repo'
+      files_paths.each do |f|
+        f = File.join directory, f
+        d = File.dirname f
+        FileUtils.rm_rf d == directory ? f : d
+      end
+    end
+
+    def local_remotes_only?
       # well... yeah, i like to make people laugh
       layer.remotes.all? { |r| File.exists? r.url }
     end
@@ -95,6 +244,23 @@ module Git
       # is there any other way ?
       # dunno for now
       File.exists? File.join(path, '.git')
+    end
+
+    def has_branch?(name)
+      # whether local or remote
+      has_local_branch?(name) || has_remote_branch?(name)
+    end
+
+    def has_local_branch?(name)
+      local_branches.include? name
+    end
+
+    def has_remote_branch?(name)
+      remote_branches.map { |b| b.split('/').last }.include? name
+    end
+
+    def any_commits?
+      local_branches.any?
     end
 
   end
--- spec/git/trifle_spec.rb
+++ spec/git/trifle_spec.rb
@@ -4,25 +4,27 @@ require 'spec_helper'
 
 describe Git::Trifle do
 
+  let(:trifle) { described_class.new }
+
+  # characteristics of the un-remote remote
+  let(:remote_url) { 'https://github.com/lacravate/git-trifle.git' }
+  let(:remote_dir) { '/tmp/spec/git-trifle/git-trifle' }
+  let(:remote_clone) { trifle.clone remote: remote_url, path: remote_dir }
+
+  # git-trifle arch'
   let(:clone_remote) { '/tmp/spec/git-trifle/try' }
   let(:public_dir) { '/tmp/spec/git-trifle/foal' }
-  let(:remote) { 'https://github.com/lacravate/git-trifle.git' }
-  let(:clone) { '/tmp/spec/git-trifle/clone' }
+  let(:remote) { remote_dir }
 
-  let(:git-trifle_options) {
-    Hash[ clone_remote: clone_remote, public_dir: public_dir, remote: remote ]
-  }
+  # setup from scratch with that
+  let(:git-trifle_options) { Hash[ clone_remote: clone_remote, public_dir: public_dir, remote: remote ] }
+  let(:git-trifle_from_scratch) { Git::Impartor.new.from_scratch git-trifle_options }
 
-  let(:git-trifle_from_scratch) {
-    impartor = Git::Impartor.new
-    # clone from github if not done already
-    impartor.trifle.clone remote: remote, path: clone unless File.exists? clone
-    # remove previous copy of clone
-    FileUtils.rm_rf clone_remote
-    # and put a fresh one
-    FileUtils.cp_r clone, clone_remote
-    # Git with wis and domous
-    impartor.from_repository git-trifle_options
+  #
+
+  before {
+    FileUtils.rm_rf remote_dir
+    remote_clone
   }
 
   describe 'cover' do
@@ -31,11 +33,11 @@ describe Git::Trifle do
     it "can open already existing repository" do
       p = subject.cover path: public_dir
       # Trifle handles the last repository it opened
-      subject.working_directory.should == public_dir
+      subject.directory.should == public_dir
 
       h = subject.cover path: clone_remote
       # Trifle handles the last repository it opened
-      subject.working_directory.should == clone_remote
+      subject.directory.should == clone_remote
 
       # Git::Base instances are different each time cover
       # is called
@@ -49,18 +51,22 @@ describe Git::Trifle do
   end
 
   describe 'init' do
-    it "inits a directory as a git repo' with a remote" do
-      subject.init remote: remote, path: clone_remote
-
-      subject.working_directory.should == '/tmp/spec/git-trifle/try'
-      subject.remote.should == remote
-    end
-
     it "inits a directory as a git repo'" do
       subject.init path: clone_remote
 
+      # this is repo'
       subject.can_cover?('/tmp/spec/git-trifle/try').should be_true
-      subject.working_directory.should == '/tmp/spec/git-trifle/try'
+      subject.directory.should == '/tmp/spec/git-trifle/try'
+      subject.remote_url.should be_nil
+    end
+
+    it "inits a directory as a git repo' with a remote" do
+      subject.init remote: remote, path: clone_remote
+
+      # how about we do a little more ?
+      subject.directory.should == '/tmp/spec/git-trifle/try'
+      # Wow ! Man !
+      subject.remote_url.should == remote
     end
   end
 
@@ -68,8 +74,9 @@ describe Git::Trifle do
     it "clones a remote repo'" do
       subject.clone remote: remote, path: clone_remote
 
+      # Ta Dah !
       subject.can_cover?('/tmp/spec/git-trifle/try').should be_true
-      subject.working_directory.should == '/tmp/spec/git-trifle/try'
+      subject.directory.should == '/tmp/spec/git-trifle/try'
     end
 
     it "accepts swicthes to clone command" do
@@ -78,65 +85,368 @@ describe Git::Trifle do
       subject.clone remote: remote, path: clone_remote, switches: ['-n']
 
       subject.can_cover?('/tmp/spec/git-trifle/try').should be_true
-      subject.working_directory.should == '/tmp/spec/git-trifle/try'
+      subject.directory.should == '/tmp/spec/git-trifle/try'
       Dir['/tmp/spec/git-trifle/try/*'].size.should == 0
+
+      # ruby git lib here, hurl that away when you can
+      subject.branch('Plop').create
+
+      # clones repo and checkout to Plop
+      # -b Plop
+      subject.clone remote: clone_remote, path: "#{clone_remote}_Plop", switches: ['-b', 'Plop']
+      subject.current_branch.should == 'Plop'
     end
   end
 
-  describe 'reset' do
-    #
-    # hurl Git lib away from the spec's
-    #
-    let(:public_repo) { Git.open public_dir }
-
-    before { 
+  describe 'checkout and branches' do
+    before {
       git-trifle_from_scratch
-
-      File.truncate File.join(public_dir, 'README.md'), 0
-      public_repo.add 'README.md'
+      subject.cover path: public_dir
+      # creates a Plop branch
+      subject.checkout 'Plop'
+      # HMV
+      subject.checkout 'master'
     }
 
-    it "performs a git reset on the handled repo'" do
-      public_repo.status.added['README.md'].type.should == 'A'
+    it "gives a list of remote branches names except current" do
+      # useful and self-explanatory
+      # otherwise i really can't do anything for you
+      subject.other_remote_branches.should == ["origin/__w_all"]
+    end
 
-      subject.cover path: public_dir
-      subject.reset # HEAD
+    it "can retrieve the remote branch name" do
+      # master was pushed on remote named origin
+      # if you have two master branches, you have
+      # a problem, buddy
+      subject.remote_branch_for('master').should == 'origin/master'
+      # nil when it dunno
+      subject.remote_branch_for('Plop').should be_nil
+    end
 
-      public_repo.status.added['README.md'].should be_nil
-      public_repo.status.changed['README.md'].type.should == 'M'
+    it "can retrieve the remote name for a branch" do
+      # same as above with remote_branch_for
+      # with same potential problem
+      subject.remote_for('master').should == 'origin'
+      # first remote when it dunno (but branch exists)
+      subject.remote_for('Plop').should == 'origin'
+      # nil when it dunno nothin', samo' samo'
+      subject.remote_for('Plap').should be_nil
+    end
+
+    it "knows whether a branch exists or not" do
+      # whether it is local or remote
+      subject.has_branch?('Plop').should be_true
+    end
+
+    it "should not blast off when ask to checkout on a repo' with no commit" do
+      FileUtils.mkdir_p '/tmp/spec/git-trifle/plop'
+      t = described_class.new
+      t.init path: '/tmp/spec/git-trifle/plop'
+      t.checkout('master').should be_false
+    end
+
+    it "should be able to switch to another existing branch" do
+      # checks out initial context
+      subject.has_branch?('Plop').should be_true
+      subject.current_branch.should == 'master'
+
+      subject.checkout 'Plop'
+
+      # i am quite a chatterbox, but i am out of words here
+      # let me call captain Obvious...
+      subject.current_branch.should == 'Plop'
+    end
+
+    it "should be able to switch to another branch it created" do
+      subject.has_branch?('Plip').should be_false
+      subject.current_branch.should == 'master'
+
+      subject.checkout 'Plip'
+
+      # another example of the DWIM
+      subject.has_branch?('Plip').should be_true
+      subject.current_branch.should == 'Plip'
+    end
+
+    it "should return the list of local branches" do
+      subject.local_branches.should =~ ['Plop', 'master', '__w_all']
+    end
+
+    it "should return the list of remote branches" do
+      subject.remote_branches.should =~ ['origin/HEAD', 'origin/master', 'origin/__w_all']
+    end
+
+    context 'branch tracking and branch pulling' do
+      before {
+        subject.cover path: public_dir
+        subject.checkout 'Ploup'
+        subject.push 'origin', 'Ploup'
+        subject.checkout 'master'
+        subject.branch('Ploup').delete
+      }
+
+      it "creates a local branch tracking a remote" do
+        # initial context check
+        current = subject.current_branch
+        subject.has_local_branch?('Ploup').should be_false
+
+        subject.create_branch 'Ploup', track: 'origin/Ploup'
+
+        # it did that without checking out to the created branch
+        current.should == subject.current_branch
+        # tracking branch is created
+        subject.has_local_branch?('Ploup').should be_true
+        subject.has_remote_branch?('Ploup').should be_true
+        subject.commits(branch: 'Ploup').should =~ subject.commits(branch: 'origin/Ploup')
+      end
+
+      it "creates a local branch tracking a remote for every remote branch" do
+        subject.pull_all_branches
+
+        # for all the remote branches, an up-to-date local one is created
+        subject.other_remote_branches.each do |branch|
+          # well, yeah... That has to be amended
+          local = branch.split('/').last
+          # tracking branch is created
+          subject.has_local_branch?(local).should be_true
+          subject.commits(branch: branch).should =~ subject.commits(branch: local)
+        end
+      end
+    end
+
+    context "checkout from a certain commit" do
+      it "should create a local branch with one commit" do
+        subject.checkout 'Plip', commit: subject.commits.first
+
+        # Yatta !
+        subject.current_branch.should == 'Plip'
+        subject.commits.size.should == 1
+      end
+    end
+
+    context "remote branch exists and local does not" do
+      before {
+        FileUtils.touch File.join(subject.directory, "Plep")
+        subject.push_file "Plep", branch: "Plop"
+        subject.checkout 'master'
+        subject.branch('Plop').delete
+      }
+
+      it "should know when local branch does not exists" do
+        # and it does
+        subject.has_local_branch?('Plop').should be_false
+      end
+
+      it "should know when remote branch exists" do
+        # and it does
+        subject.has_remote_branch?('Plop').should be_true
+      end
+
+      it "should know when branch exists, remote or local" do
+        # and it does
+        subject.has_branch?('Plop').should be_true
+      end
+
+      it "should create a tracking branch when remote branch exists" do
+         subject.checkout 'Plop'
+
+         # DWIM again
+         # simple checkout with a branch name reflecting a remote one
+         # creates a tracking one
+         subject.has_local_branch?('Plop').should be_true
+         subject.commits.size.should == 2
+      end
+    end
+
+    context 'checkout_w_branch' do
+      before {
+        FileUtils.touch File.join(subject.directory, "Plep")
+        subject.push_file "Plep", branch: "Plop"
+        subject.checkout 'master'
+        subject.branch('Plop').delete
+      }
+
+      it "should create a new branch with one initial commit" do
+        # git-trifle branch are named differently
+        # but it's not a design so far, and it's irrelevant here
+        subject.checkout_w_branch 'Plip'
+        subject.current_branch.should == 'Plip'
+        # design is there, a git-trifle branch stems from the initial commit
+        subject.commits.size.should == 1
+        subject.commits.first.should == subject.commits(branch: 'master').first
+      end
+
+      it "should checkout a branch tracking the remote one of the same name" do
+        subject.has_local_branch?('Plop').should be_false
+
+        subject.checkout_w_branch 'Plop'
+
+        # checkout_w_branch behaves like a mere checkout when
+        # remote branch exists
+        subject.current_branch.should == 'Plop'
+        subject.commits.size.should == 2
+        subject.commits.last.should_not == subject.commits(branch: 'master').first
+        subject.commits(branch: 'origin/Plop').should =~ subject.commits
+      end
     end
   end
 
-  describe 'working_directory' do
+  describe 'status and alterations' do
+    before {
+      git-trifle_from_scratch
+      File.truncate File.join(public_dir, 'README.md'), 0
+      FileUtils.touch File.join(public_dir, 'Plopinou')
+    }
+
+    it "should be able to get the current status of a repo'" do
+      subject.cover path: public_dir
+      # files status as a Hash
+      subject.status.should ==  { changed: ["README.md"], added: [], deleted: [], untracked: ['Plopinou'] }
+      # a chosen subset of it
+      subject.status(:changed).should == { changed: ["README.md"] }
+    end
+
+    it "should give a list of files according to git status" do
+      subject.cover path: public_dir
+
+      subject.cover path: public_dir
+      # other subsets of the status, returning only the files list
+      subject.files_with_status(:untracked).should == ['Plopinou']
+      subject.files_with_status(:added).should == []
+      subject.files_with_status(:changed).should == ['README.md']
+      subject.files_with_status(:deleted).should == []
+    end
+
+    it "should list the alterations by type" do
+      # same as before, except that the filter is on files presence in lists
+      subject.cover path: public_dir
+      subject.alterations.should == { untracked: ['Plopinou'], changed: ['README.md'] }
+      # another subset of the above-mentionned subset
+      subject.alterations(status: :changed).should == { changed: ['README.md'] }
+    end
+  end
+
+  describe 'push file' do
+    before {
+      git-trifle_from_scratch
+
+      subject.cover path: public_dir
+      subject.checkout 'Plop'
+      File.truncate File.join(public_dir, 'README.md'), 0
+      # add, commit, and push a file alteration to a new branch
+      subject.push_file 'README.md'
+    }
+
+    it "adds a file, commits and push the alteration on the remote" do
+      # and it shows
+      subject.commits.size.should == 2
+      # nothing left to be done
+      subject.alterations.should == {}
+      # pushed
+      subject.has_remote_branch?('Plop').should be_true
+      subject.commits(branch: 'origin/Plop').should =~ subject.commits
+    end
+  end
+
+  describe 'directory' do
     before { git-trifle_from_scratch }
 
     it "should be able to give the repo working directory" do
+      # bleeding edge feature... Trifle instance knows how to
+      # render a parameter passed to one of its methods
       subject.cover path: '/tmp/spec/git-trifle/try'
-      subject.working_directory.should == '/tmp/spec/git-trifle/try'
+      subject.directory.should == '/tmp/spec/git-trifle/try'
       subject.cover path:'/tmp/spec/git-trifle/foal'
-      subject.working_directory.should == '/tmp/spec/git-trifle/foal'
+      subject.directory.should == '/tmp/spec/git-trifle/foal'
     end
   end
 
-  describe 'remote' do
+  describe 'remote_url' do
     before { git-trifle_from_scratch }
 
     it "should return the first, possibly matching, remote url" do
       subject.cover path: '/tmp/spec/git-trifle/try'
-      subject.remote.should == remote
-      subject.remote('origin').should == remote
-      subject.remote('plop').should be_nil
+      # finds the remote url, gives away the first one if several are
+      # found and you were not specific enough
+      subject.remote_url.should == remote
+      # now you are specific enough
+      subject.remote_url(name: 'origin').should == remote
+      # dunno that one buddy...
+      subject.remote_url(name: 'plop').should be_nil
+      # gold fish memory
       subject.cover path:'/tmp/spec/git-trifle/foal'
-      subject.remote.should == '/tmp/spec/git-trifle/try'
+      subject.remote_url.should == '/tmp/spec/git-trifle/try'
+    end
+
+    it "should be able to retrieve the remote name" do
+      subject.cover path: '/tmp/spec/git-trifle/try'
+      # once more, not specific enough buddy, i did what i could
+      subject.remote_name.should == 'origin'
+      # there you go !
+      subject.remote_name(url: '/tmp/spec/git-trifle/git-trifle').should == 'origin'
+      # there you go... away.
+      subject.remote_name(url: 'plop').should be_nil
     end
   end
 
-  describe 'files_paths' do
+  describe 'commits' do
     before { git-trifle_from_scratch }
+    let(:lousy_commit) {
+      File.truncate File.join(public_dir, 'README.md'), 0
+      subject.add 'README.md'
+      subject.commit "lousy test commit"
+    }
+
+    it "lists the commits chronologically" do
+      # check initial context
+      subject.cover path: public_dir
+      first = subject.commits.first
+      subject.commits.size.should == 1
+
+      # lousy commit
+      lousy_commit
+
+      # everything's there, represented the way
+      # i want and understand
+      subject.commits.size.should == 2
+      subject.commits.first.should == first
+    end
+
+    it "lists the commits of another branch" do
+      subject.cover path: public_dir
+
+      lousy_commit
+
+      subject.checkout 'Plop'
+
+      subject.current_branch.should == 'Plop'
+      subject.commits.size.should == 2
+      # incredible feature isn't it ?
+      # but it's useful
+      subject.commits(branch: '__w_all').size.should == 2
+    end
+  end
+
+  describe 'files_paths related' do
+    before {
+      git-trifle_from_scratch
+      FileUtils.cp_r File.join(RSpec.configuration.fixtures_dir, 'files', 'Plip'), clone_remote
+      # subject not available here ? Ok...
+      # i like tap... Though it's perfectly unnecessary here. But i like it.
+      described_class.new.tap { |t| t.cover path: '/tmp/spec/git-trifle/try'; t.add 'Plip' }
+    }
 
     it "should give all the files tracked by git" do
       subject.cover path: '/tmp/spec/git-trifle/try'
-      subject.files_paths.should =~ ['README.md']
+      # lists the files, not the directories
+      subject.files_paths.should =~ ['README.md', "Plip/Plap", "Plip/Plop"]
+    end
+
+    it "should wipe out all file in directory" do
+      subject.cover path: '/tmp/spec/git-trifle/try'
+      # everything must disappear
+      subject.wipe_directory!
+      Dir['/tmp/spec/git-trifle/try/*'].empty?.should be_true
     end
   end
 
@@ -144,19 +454,20 @@ describe Git::Trifle do
     before { git-trifle_from_scratch }
 
     it "should be able to tell if a path given holds a git repository" do
+      # to avoid blowing out in the ether, dirty, when false
       subject.can_cover?('/tmp/spec/git-trifle/try').should be_true
       subject.can_cover?('/tmp').should be_false
     end
   end
 
-  describe 'local_remotes?' do
+  describe 'local_remotes_only?' do
     before { git-trifle_from_scratch }
 
     it "should be able to tell if a path given holds a git repository" do
-      subject.cover path: '/tmp/spec/git-trifle/try'
-      subject.local_remotes?.should be_false
+      # another a teenily tinily little shrivel short scope method
+      # to wrap this select/map/reject/inject fest
       subject.cover path: '/tmp/spec/git-trifle/foal'
-      subject.local_remotes?.should be_true
+      subject.local_remotes_only?.should be_true
     end
   end
 
